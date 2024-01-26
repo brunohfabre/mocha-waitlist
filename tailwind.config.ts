4c119c1b2988b6c15cbce025aa40d443aec98419
Bruno Fabre <bruno.hfabre@gmail.com>
2024-01-26 10:24:19 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          400: '#7C7C8A',
+          500: '#505059',
+          600: '#323238',
+          700: '#29292E',
+          800: '#202024',
+          900: '#121214',
+          950: '#09090A',
+        },
+
+        'mocha-orange': '#FFA161',
+        'mocha-pink': '#F261A7',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+}
+export default config
